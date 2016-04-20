96f92ccf885d6adac6cc3f403e01800405ff270b
Tony <tonywang3334@gmail.com>
2016-04-20 17:34:37 +0200

    fixade bugg i similar ifall inga matches

--- Python/Similar.py
+++ Python/Similar.py
@@ -8,7 +8,7 @@ def similarwords(word):
     oldstdout = sys.stdout
     sys.stdout = open("out.txt", "w+")
     
-    moby = Text(corpus.gutenberg.words('melville-moby_dick.txt')) #CORPORA ON THIS LINE
+    moby = Text(corpus.gutenberg.words('milton-paradise.txt')) #CORPORA ON THIS LINE
     t = moby.similar(word) #GET SIMILAR WORDS FROM CORPORA
     
     sys.stdout = oldstdout
@@ -16,9 +16,14 @@ def similarwords(word):
     txt = open('out.txt', 'r')
     similarwords =  word_tokenize(txt.read())
         
- #   print(similarwords)
+    print(similarwords)
     
     random_upper_treshold = len(similarwords)
+    print(random_upper_treshold)
+    if (random_upper_treshold < 3):
+        random_upper_treshold = 3
+
+    
     random_similarwords = [0,0,0]
     random_numbers = [0,0,0]
     random_numbers[0] = random.randint(0, random_upper_treshold) #firsta random vardet
@@ -35,18 +40,19 @@ def similarwords(word):
             if(thirdrandom != random_numbers[1]): #fostod in and i python gjorde tva if satser istallet
                 random_numbers[2] = thirdrandom
                 break
-    
-    for x in range (0, 3): #stoppa in de tre random similar orden i en lista
-        random_similarwords[x] = similarwords[random_numbers[x]]
-#    print(random_similarwords)
-    for x in range (0, 3):
-        if(random_similarwords[x] == 0):
+    if(len(similarwords) > 2):
+        for x in range (0, 3): #stoppa in de tre random similar orden i en lista
+            random_similarwords[x] = similarwords[random_numbers[x]]
+            print(random_similarwords)
+        for x in range (0, 3): #kanske inte behover denna loop
+            if(random_similarwords[x] == 0):
                 random_similarwords[x] = 'Not generated' 
+
+    else: random_similarwords = ['Not generated', 'Not generated', 'Not generated']
     return random_similarwords
-
 def main():
-
-    similarwords("captain")
+    
+    print(similarwords("captain"))
     
 
 
